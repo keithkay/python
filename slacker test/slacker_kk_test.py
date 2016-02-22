a5903bd72751bcd77c065dc391290b1256b35f1c
Keith Kay <me@keithkay.com>
2016-02-22 12:07:46 -0700

    Add icon_emoji
    Add icon_emoji to the chat post

--- slacker test/slacker_kk_test.py
+++ slacker test/slacker_kk_test.py
@@ -1,3 +1,4 @@
+# written on python 2.7.11
 # test of slacker Python-Slack integration
 # slacker: https://github.com/os/slacker
 # slack api: https://api.slack.com/methods
@@ -7,7 +8,17 @@ from slacker import Slacker
 slack = Slacker('xoxp-2153792019-8313249396-20534467781-31e3a14fe0')
 
 # Send a message to #[channel/group]
-slack.chat.post_message('#kks-python-test', 'test post', username='appathon bot', as_user=False)
+slack.chat.post_message('#kks-python-test',
+                        'test post:slightly_smiling_face:',
+                        username='timekeeper',
+                        as_user=False,
+                        icon_emoji=':stopwatch:')
+
+slack.chat.post_message('#kks-python-test',
+                        '/topic test setting topic',
+                        username='timekeeper',
+                        as_user=False,
+                        icon_emoji=':stopwatch:')
 
 # Set the purpose to a group
 #slack.groups.set_purpose("#kks-python-test", "test purpose")
