c263d358092a357bd6a3d4b28ae079a0bbc7567e
Keith Kay <me@keithkay.com>
2016-04-29 15:05:26 -0600

    Added working directory for course
    Added a working directory (sandbox) for the Python Crash Course files

---
+++ sandbox/variables.py
@@ -0,0 +1,19 @@
+# strings
+
+name = "ada lovelace"
+print(name.title())
+
+first_name = "ada"
+last_name = "lovelace"
+
+full_name = first_name + " " + last_name
+
+print(full_name)
+
+# whitespace
+
+print("\tI have a tab before me")
+print("Here is a new line:\nsee!")
+print("Here is a new line and a tab:\n\twoo-hoo!")
+
+
