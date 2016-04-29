d5877f975972d2f8d573841dce0d6430a1660616
Keith Kay <keithkay@users.noreply.github.com>
2016-04-29 15:34:04 -0600

    Initial slack timekeeper dev (#4) (#5)
    * Added working directory for course
    
    Added a working directory (sandbox) for the Python Crash Course files
    
    * Chapter 3 - Lists

---
+++ sandbox/lists.py
@@ -0,0 +1,14 @@
+# python lists
+
+# use purals for list names
+
+bicycles = ['trek', 'cannondale','redline','specialized'] #brackets signify this is a list
+print(bicycles) 		# prints the representation of the list, not a formatted list
+
+for bike in bicycles:	# prints a nice list
+	print bike
+
+# accessing specific items in the list
+
+print(bicycles[0])		# first
+print(bicycles[-1])		# last item
